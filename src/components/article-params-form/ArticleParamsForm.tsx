c3d522c33081fe75f68435682a1a27af353c231e
unknown <verkhovp@yandex.ru>
2025-11-14 14:32:50 +0300

    sidebar finished

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,139 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import { Select } from 'src/ui/select';
+import {
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { Text } from 'src/ui/text';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { setPage } from 'src/index';
 
-export const ArticleParamsForm = () => {
+type TFormProps = {
+	mainRef: React.RefObject<HTMLElement>;
+};
+export const ArticleParamsForm = (props: TFormProps) => {
+	const [isFormOpen, setFormOpen] = useState(false);
+	const [formState, setFormState] = useState({
+		fontSelected: defaultArticleState.fontFamilyOption,
+		fontSizeSelected: defaultArticleState.fontSizeOption,
+		fontColorSelected: defaultArticleState.fontColor,
+		bgColorSelected: defaultArticleState.backgroundColor,
+		selectedContentWidth: defaultArticleState.contentWidth,
+	});
+
+	const mainElement = props.mainRef.current;
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={isFormOpen}
+				onClick={() => {
+					setFormOpen((prevValue) => !prevValue);
+				}}
+			/>
+			<aside
+				className={clsx(
+					styles.container,
+					isFormOpen ? styles.container_open : ''
+				)}>
 				<form className={styles.form}>
+					<div className={styles.title}>
+						<Text size={31} weight={800} uppercase>
+							{'Задайте параметры'}
+						</Text>
+					</div>
+					<Select
+						options={fontFamilyOptions}
+						selected={formState.fontSelected}
+						title='Шрифт'
+						onChange={(option) => {
+							setFormState({ ...formState, fontSelected: option });
+						}}
+					/>
+					<RadioGroup
+						name='fontSize'
+						options={fontSizeOptions}
+						selected={formState.fontSizeSelected}
+						title='размер шрифта'
+						onChange={(option) => {
+							setFormState({ ...formState, fontSizeSelected: option });
+						}}
+					/>
+					<Select
+						options={fontColors}
+						selected={formState.fontColorSelected}
+						title='цвет шрифта'
+						onChange={(option) => {
+							setFormState({ ...formState, fontColorSelected: option });
+						}}
+					/>
+					<Separator />
+
+					<Select
+						options={backgroundColors}
+						selected={formState.bgColorSelected}
+						title='цвет фона'
+						onChange={(option) =>
+							setFormState({ ...formState, bgColorSelected: option })
+						}
+					/>
+					<Select
+						options={contentWidthArr}
+						selected={formState.selectedContentWidth}
+						title='Ширина контента'
+						onChange={(option) => {
+							setFormState({ ...formState, selectedContentWidth: option });
+						}}
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={() => {
+								setPage(
+									mainElement,
+									defaultArticleState.fontFamilyOption.value,
+									defaultArticleState.fontSizeOption.value,
+									defaultArticleState.fontColor.value,
+									defaultArticleState.contentWidth.value,
+									defaultArticleState.backgroundColor.value
+								);
+								setFormState({
+									fontSelected: defaultArticleState.fontFamilyOption,
+									fontSizeSelected: defaultArticleState.fontSizeOption,
+									fontColorSelected: defaultArticleState.fontColor,
+									bgColorSelected: defaultArticleState.backgroundColor,
+									selectedContentWidth: defaultArticleState.contentWidth,
+								});
+							}}
+						/>
+
+						<Button
+							title='Применить'
+							htmlType='submit'
+							type='apply'
+							onClick={(e) => {
+								e.preventDefault();
+								setPage(
+									mainElement,
+									formState.fontSelected.value,
+									formState.fontSizeSelected.value,
+									formState.fontColorSelected.value,
+									formState.selectedContentWidth.value,
+									formState.bgColorSelected.value
+								);
+							}}
+						/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,33 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useRef } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
 import { defaultArticleState } from './constants/articleProps';
-
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+export function setPage(
+	element: HTMLElement | null,
+	fontFamily: string,
+	fontSize: string,
+	fontColor: string,
+	containerWidth: string,
+	bgColor: string
+) {
+	element?.style.setProperty('--font-family', fontFamily);
+	element?.style.setProperty('--font-size', fontSize);
+	element?.style.setProperty('--font-color', fontColor);
+	element?.style.setProperty('--container-width', containerWidth);
+	element?.style.setProperty('--bg-color', bgColor);
+}
+
 const App = () => {
+	const mainRef = useRef<HTMLElement>(null);
 	return (
 		<main
 			className={clsx(styles.main)}
@@ -24,8 +39,9 @@ const App = () => {
 					'--container-width': defaultArticleState.contentWidth.value,
 					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+			}
+			ref={mainRef}>
+			<ArticleParamsForm mainRef={mainRef} />
 			<Article />
 		</main>
 	);
